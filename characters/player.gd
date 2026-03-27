649e80aa07d8ecf6f9e27711adad900be579504a
juliohq <JulioHenrique31501234@gmail.com>
2026-03-27 17:12:54 -0300

    Add jump buffering

--- characters/player.gd
+++ characters/player.gd
@@ -5,6 +5,9 @@ extends CharacterBody2D
 @export_range(1, 100, 1, "or_greater", "suffix:px/s") var movement_speed := 128
 ## How high the character will jump.
 @export_range(1, 100, 1, "or_greater", "suffix:px") var jump_height := 384
+## How long a jump will be accepted.
+@export_range(0.01, 1.0, 0.01, "or_greater", "suffix:s")
+var jump_buffering := 0.2
 @export_category("Nodes")
 @export var sprite: Sprite2D
 @export var state_machine: FiniteStateMachine
@@ -12,6 +15,8 @@ extends CharacterBody2D
 
 ## The horizontal direction the player is moving to.
 var direction := 0.0
+## The time left to jump.
+var jump_buffer := 0.0
 
 @onready var gravity = ProjectSettings.get_setting_with_override(&"physics/2d/default_gravity")
 
@@ -23,13 +28,17 @@ func _physics_process(delta: float) -> void:
 	elif direction > 0.0:
 		sprite.flip_h = false
 	
-	# Jump logic
-	if Input.is_action_just_pressed(&"jump") and is_on_floor():
-		jump()
-		move_and_slide()
+	# Jump logic (with jump buffering)
+	if Input.is_action_just_pressed(&"jump"):
+		jump_buffer = jump_buffering
 	# Variable jump height logic
 	elif Input.is_action_just_released(&"jump") and velocity.y < 0.0:
 		fall()
+	
+	if is_on_floor() and jump_buffer > 0.0:
+		jump_buffer = 0.0
+		jump()
+		move_and_slide()
 	
 	# Uncomment to enable
 	# Note: Remember to disable the same logic in _unhandled_input
@@ -45,6 +54,12 @@ func _physics_process(delta: float) -> void:
 		velocity.y = 0.0
 	else:
 		velocity.y += gravity * delta
+	
+	# Jump buffer
+	if jump_buffer > 0.0:
+		jump_buffer -= delta
+	else:
+		jump_buffer = 0.0
 	
 	# Perform the actual movement
 	move_and_slide()
