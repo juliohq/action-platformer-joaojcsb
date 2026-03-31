8db699eec7d0f315a7414e7dd958c6d3f18ed3ff
juliohq <JulioHenrique31501234@gmail.com>
2026-03-31 19:35:47 -0300

    Add orbs

---
+++ ui/orb.gd
@@ -0,0 +1,28 @@
+extends MarginContainer
+
+
+const BLUE_ORB := [
+	preload("res://assets/ui/blue_orb/0.png"),
+	preload("res://assets/ui/blue_orb/1.png"),
+	preload("res://assets/ui/blue_orb/2.png"),
+	preload("res://assets/ui/blue_orb/3.png"),
+	preload("res://assets/ui/blue_orb/4.png"),
+	preload("res://assets/ui/blue_orb/5.png"),
+	preload("res://assets/ui/blue_orb/6.png"),
+]
+const RED_ORB := [
+	preload("res://assets/ui/red_orb/0.png"),
+	preload("res://assets/ui/red_orb/1.png"),
+	preload("res://assets/ui/red_orb/2.png"),
+	preload("res://assets/ui/red_orb/3.png"),
+	preload("res://assets/ui/red_orb/4.png"),
+	preload("res://assets/ui/red_orb/5.png"),
+	preload("res://assets/ui/red_orb/6.png"),
+]
+
+
+func _ready() -> void:
+	%Orb.texture = [
+		BLUE_ORB,
+		RED_ORB,
+	][get_index()][0]
