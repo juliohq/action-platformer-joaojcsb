19e1202fcd762481bf69b26ce02ec511d6b8aeab
juliohq <JulioHenrique31501234@gmail.com>
2026-04-07 18:14:26 -0300

    Add orb bounce

--- scenes/orb.gd
+++ scenes/orb.gd
@@ -1,13 +1,14 @@
 extends CharacterBody2D
 
 
-@export_range(0.1, 10.0, 0.1, "or_greater", "suffix:px/s²") var gravity := 9.8
+@export_range(0.1, 10.0, 0.1, "or_greater", "suffix:px/s²") var gravity := 980
+@export_range(0.1, 10.0, 0.1, "or_greater", "suffix:px/s") var bounce := 360
 
 
 func _physics_process(delta: float) -> void:
 	if is_on_floor():
 		# Bounce
-		velocity.y = -absf(velocity.y)
+		velocity.y = -bounce
 	else:
 		# Gravity
 		velocity.y += gravity * delta
