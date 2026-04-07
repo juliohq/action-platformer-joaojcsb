8116ebe2d58bb994a00d77248065e5bd2036a5d5
juliohq <JulioHenrique31501234@gmail.com>
2026-04-07 18:10:11 -0300

    Add orb gravity

--- scenes/orb.gd
+++ scenes/orb.gd
@@ -1,13 +1,16 @@
 extends CharacterBody2D
 
 
+@export_range(0.1, 10.0, 0.1, "or_greater", "suffix:px/s²") var gravity := 9.8
+
+
 func _physics_process(delta: float) -> void:
 	if is_on_floor():
 		# Bounce
 		velocity.y = -absf(velocity.y)
 	else:
 		# Gravity
-		velocity = Vector2.DOWN * delta
+		velocity.y += gravity * delta
 	
 	# Movement
 	move_and_slide()
