2e4dfdfc98d34a41b124b1718b745f6a3976d314
juliohq <JulioHenrique31501234@gmail.com>
2026-04-07 18:48:14 -0300

    Add floor detection for checkpoint

--- scenes/checkpoint.gd
+++ scenes/checkpoint.gd
@@ -3,6 +3,10 @@ extends Node
 
 ## The parent node.
 @export var parent: Node2D
+## The first raycast for floor detection.
+@export var raycast_a: RayCast2D
+## The second raycast for floor detection.
+@export var raycast_b: RayCast2D
 ## The X axis will be used.
 @export var enable_x := true
 ## The X axis will be positive.
@@ -17,17 +21,29 @@ var position := Vector2()
 
 
 func _physics_process(_delta: float) -> void:
+	var on_floor := raycast_a.is_colliding() and raycast_b.is_colliding()
+	
 	if enable_x:
-		if positive_x:
-			position.x = maxf(parent.position.x, position.x)
-		else:
-			position.x = minf(parent.position.x, position.x)
+		if on_floor:
+			store_x()
 	
 	if enable_y:
-		if positive_y:
-			position.y = maxf(parent.position.y, position.y)
-		else:
-			position.y = minf(parent.position.y, position.y)
+		if on_floor:
+			store_y()
+
+
+func store_x() -> void:
+	if positive_x:
+		position.x = maxf(parent.position.x, position.x)
+	else:
+		position.x = minf(parent.position.x, position.x)
+
+
+func store_y() -> void:
+	if positive_y:
+		position.y = maxf(parent.position.y, position.y)
+	else:
+		position.y = minf(parent.position.y, position.y)
 
 
 ## Restores the player position stored in this checkpoint.
