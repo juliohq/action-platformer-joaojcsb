f0251176cca58033dd5f5d54e167bb00034158c3
juliohq <JulioHenrique31501234@gmail.com>
2026-04-12 16:59:11 -0300

    Add random loot drop count

--- scenes/loot.gd
+++ scenes/loot.gd
@@ -2,10 +2,13 @@ extends Node2D
 
 
 @export var scene: PackedScene
-@export_range(1, 10, 1, "or_greater") var count := 1
+@export_range(1, 10, 1, "or_greater") var min_count := 1
+@export_range(1, 10, 1, "or_greater") var max_count := 1
 
 
 func drop() -> void:
+	var count := randi_range(min_count, max_count)
+	
 	for i in count:
 		var loot := scene.instantiate()
 		loot.position = position
