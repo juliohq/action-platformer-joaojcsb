3a298eaa8c77404dc3506fe2092ea40f4018e015
juliohq <JulioHenrique31501234@gmail.com>
2026-04-18 19:39:36 -0300

    Add enemy knockback

--- enemies/enemy/enemy.gd
+++ enemies/enemy/enemy.gd
@@ -5,6 +5,11 @@ extends CharacterBody2D
 @export_range(1, 10, 1, "or_greater", "suffix:HP") var max_health := 3
 ## How fast the character will move along the X axis.
 @export_range(1, 100, 1, "or_greater", "suffix:px/s") var movement_speed := 128
+## How much is the default knockback on the X axis.
+@export_range(1, 100, 1, "or_greater", "suffix:px/s")
+var default_knockback := 128
+## How fast the knockback will reset.
+@export_range(1, 100, 1, "or_greater", "suffix:px/s") var knockback_speed := 64
 @export_category("Nodes")
 @export var hit_box: Area2D
 
@@ -12,6 +17,8 @@ extends CharacterBody2D
 var health := max_health
 ## The horizontal direction the player is moving to.
 var direction := 0.0
+## The current knockback.
+var knockback := 0.0
 
 @onready var gravity = ProjectSettings.get_setting_with_override(&"physics/2d/default_gravity")
 
@@ -30,6 +37,10 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.y += gravity * delta
 	
+	# Knockback
+	knockback = move_toward(knockback, 0.0, knockback_speed * delta)
+	velocity.x += knockback
+	
 	# Perform the actual movement
 	move_and_slide()
 
@@ -39,7 +50,7 @@ func hit(damage: int) -> void:
 	
 	if health > 0:
 		# Knockback
-		pass
+		knockback = default_knockback
 	else:
 		# Death logic here
 		queue_free()
