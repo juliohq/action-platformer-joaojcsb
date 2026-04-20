797c5ddd8a7d7e98e78ffaaa0b3ea7fc68ade2f6
juliohq <JulioHenrique31501234@gmail.com>
2026-04-20 16:21:41 -0300

    Add fireball hitbox

--- scenes/bullet.gd
+++ scenes/bullet.gd
@@ -1,19 +1,26 @@
-extends Area2D
+extends Node2D
 
 
 ## How fast the bullet will move.
 @export_range(1, 128, 1, "or_greater", "suffix:px/s") var speed := 128
 @export_category("Nodes")
 @export var sprite: Sprite2D
+@export var hit_box: Area2D
 
 ## The current direction of the bullet.
 var direction := Vector2()
 
 
 func _ready() -> void:
+	hit_box.hit.connect(_hit)
+	
 	# Rotate the sprite
 	sprite.rotation = direction.angle()
 
 
 func _physics_process(delta: float) -> void:
 	translate(direction * speed * delta)
+
+
+func _hit(_body: Node2D) -> void:
+	queue_free()
