9c8049bd2f0b1cd6d2335d7e0986e8952933e4f5
juliohq <JulioHenrique31501234@gmail.com>
2026-04-21 17:45:54 -0300

    Fix bound checks too often

--- enemies/enemy/states/walk.gd
+++ enemies/enemy/states/walk.gd
@@ -1,8 +1,13 @@
 extends BaseState
 
 
+const BOUND_CHECK := 0.05
+
 @export var sprite: Sprite2D
 @export var surface_detector: Node2D
+
+# Fix bounds checks too often
+var time_left := 0.0
 
 
 func on_enter() -> void:
@@ -10,9 +15,14 @@ func on_enter() -> void:
 	root.direction = signf(randf() - 0.5)
 
 
-func on_physics_process(_delta: float) -> void:
-	if surface_detector.bound_reached():
-		root.direction *= -1
+func on_physics_process(delta: float) -> void:
+	if time_left > 0.0:
+		time_left -= delta
+	else:
+		time_left += BOUND_CHECK
+		
+		if surface_detector.bound_reached():
+			root.direction *= -1
 	
 	# Flip sprite
 	if root.direction > 0.0:
