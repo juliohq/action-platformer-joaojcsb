d3a9a1b4be0a6efad1d85f28c2e38fdce3d3e7dd
juliohq <JulioHenrique31501234@gmail.com>
2026-04-21 07:57:23 -0300

    Add melee hitbox

--- scenes/hit_box.gd
+++ scenes/hit_box.gd
@@ -4,15 +4,27 @@ extends Area2D
 ## Emitted when it hurts the player.
 signal hit(body)
 
+## Apply damage when a body enters the hit box.
+## otherwise, attack will be manual.
+@export var on_enter := true
 ## How much health this hitbox will take from enemies.
 @export_range(1, 10, 1, "or_greater", "suffix:HP") var attack := 1
 
 
 func _ready() -> void:
-	body_entered.connect(_hit)
+	if on_enter:
+		body_entered.connect(_hit)
 
 
 func _hit(body: Node2D) -> void:
 	if body.has_method("hit"):
 		body.hit(attack)
 		hit.emit(body)
+
+
+func damage() -> void:
+	for body: Node2D in get_overlapping_bodies():
+		if body.has_method("hit"):
+			body.hit(attack)
+			hit.emit(body)
+			return
