e8fc56fb49b20a554c72317466be0c900ecf37d8
juliohq <JulioHenrique31501234@gmail.com>
2026-04-21 18:56:19 -0300

    Add orb pick up logic

--- scenes/orb.gd
+++ scenes/orb.gd
@@ -5,10 +5,12 @@ extends CharacterBody2D
 @export_range(0.1, 10.0, 0.1, "or_greater", "suffix:px/s") var bounce := 300
 @export_category("Nodes")
 @export var vanish: Timer
+@export var collectible: Area2D
 
 
 func _ready() -> void:
 	vanish.timeout.connect(queue_free)
+	collectible.picked_up.connect(_picked_up)
 
 
 func _physics_process(delta: float) -> void:
@@ -21,3 +23,7 @@ func _physics_process(delta: float) -> void:
 	
 	# Movement
 	move_and_slide()
+
+
+func _picked_up() -> void:
+	queue_free()
