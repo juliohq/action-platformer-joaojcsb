bce93e16539f932bbc18c8c50fe3a0552b9eaa35
juliohq <JulioHenrique31501234@gmail.com>
2026-04-28 19:45:25 -0300

    Add grape shoot

---
+++ scenes/shoot.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+
+## The scene to be spawned.
+@export var scene: PackedScene
+## How fast to shoot.
+@export_range(0.01, 10.0, 0.01, "or_greater", "suffix:units/s")
+var fire_rate := 1.0
+@export_category("Nodes")
+@export var root: Node2D
+
+## How fast to shoot.
+var cooldown := 0.0
+
+
+func _physics_process(delta: float) -> void:
+	if cooldown > 0.0:
+		cooldown -= delta
+	else:
+		cooldown += 1.0 / fire_rate
+		shoot()
+
+
+func shoot() -> void:
+	var bullet := scene.instantiate()
+	bullet.direction.x = root.direction
+	bullet.global_position = global_position
+	Events.bullet.emit(bullet)
