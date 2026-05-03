0c1d4e7bfe59604455ce635e38cadadc9db0dfc5
juliohq <JulioHenrique31501234@gmail.com>
2026-05-03 11:15:03 -0300

    Add grape jump logic

--- enemies/grape/states/idle.gd
+++ enemies/grape/states/idle.gd
@@ -1,6 +1,8 @@
 extends BaseState
 
 
+const JUMP_HEIGHT := 256
+
 @export var sprite: Sprite2D
 @export var player_detector: Area2D
 
@@ -10,6 +12,7 @@ func on_enter() -> void:
 
 
 func on_physics_process(_delta: float) -> void:
+	# Flip sprite
 	for player: Node2D in player_detector.get_overlapping_bodies():
 		if root.position.x > player.position.x:
 			sprite.flip_h = not root.sprite_faces_left
@@ -17,3 +20,12 @@ func on_physics_process(_delta: float) -> void:
 			sprite.flip_h = root.sprite_faces_left
 		
 		break
+	
+	# Jump
+	jump()
+
+
+func jump() -> void:
+	if root.is_on_floor():
+		root.velocity.y = -JUMP_HEIGHT
+		root.move_and_slide()
