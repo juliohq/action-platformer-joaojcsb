778c90fc5de557d15928d45d2b553ee5b986d262
juliohq <JulioHenrique31501234@gmail.com>
2026-05-03 11:30:01 -0300

    Disable hurt box when bushly is camouflaged

--- enemies/bushly/states/camouflaged.gd
+++ enemies/bushly/states/camouflaged.gd
@@ -1,12 +1,19 @@
 extends BaseState
 
 
+const ENEMY_LAYER := 4
+
 @export var idle: BaseState
 @export var player_detector: Area2D
 
 
 func on_enter() -> void:
 	animator.play("CAMOUFLAGED")
+	root.set_collision_layer_value(ENEMY_LAYER, false)
+
+
+func on_exit() -> void:
+	root.set_collision_layer_value(ENEMY_LAYER, true)
 
 
 func on_physics_process(_delta: float) -> void:
