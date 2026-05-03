38bae99c2de5eb6955f23e565de037ff9f7865f2
juliohq <JulioHenrique31501234@gmail.com>
2026-05-03 17:54:45 -0300

    Add grape jump interval

--- enemies/grape/states/idle.gd
+++ enemies/grape/states/idle.gd
@@ -2,16 +2,20 @@ extends BaseState
 
 
 const JUMP_HEIGHT := 256
+const JUMP_INTERVAL := 0.5
 
 @export var sprite: Sprite2D
 @export var player_detector: Area2D
+
+## Time left until jump again.
+var time_left := 0.0
 
 
 func on_enter() -> void:
 	animator.play("IDLE")
 
 
-func on_physics_process(_delta: float) -> void:
+func on_physics_process(delta: float) -> void:
 	# Flip sprite
 	for player: Node2D in player_detector.get_overlapping_bodies():
 		if root.position.x > player.position.x:
@@ -22,10 +26,14 @@ func on_physics_process(_delta: float) -> void:
 		break
 	
 	# Jump
-	jump()
+	jump(delta)
 
 
-func jump() -> void:
+func jump(delta: float) -> void:
 	if root.is_on_floor():
-		root.velocity.y = -JUMP_HEIGHT
-		root.move_and_slide()
+		if time_left > 0.0:
+			time_left -= delta
+		else:
+			time_left = JUMP_INTERVAL
+			root.velocity.y = -JUMP_HEIGHT
+			root.move_and_slide()
