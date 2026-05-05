594c337fe1f845cd36bb1f2a87081bbdcafd8fa9
juliohq <JulioHenrique31501234@gmail.com>
2026-05-05 18:54:50 -0300

    Remove coins that fall offscreen

--- scenes/coin.gd
+++ scenes/coin.gd
@@ -5,10 +5,12 @@ extends CharacterBody2D
 @export_range(0.1, 10.0, 0.1, "or_greater", "suffix:px/s²") var gravity := 980
 @export_category("Nodes")
 @export var collectible: Area2D
+@export var visibility: VisibleOnScreenNotifier2D
 
 
 func _ready() -> void:
 	collectible.picked_up.connect(_picked_up)
+	visibility.screen_exited.connect(_screen_exited)
 
 
 func _physics_process(delta: float) -> void:
@@ -26,3 +28,8 @@ func _picked_up() -> void:
 	Globals.coins += 1
 	Events.coins_changed.emit()
 	queue_free()
+
+
+func _screen_exited() -> void:
+	if velocity.y > 0.0:
+		queue_free()
