f7b98b2a546924731a2a3ce484dae7ce1d5c53e7
juliohq <JulioHenrique31501234@gmail.com>
2026-05-07 17:51:22 -0300

    Add player hit effect

--- scenes/level.gd
+++ scenes/level.gd
@@ -16,3 +16,4 @@ func _ready() -> void:
 	Events.hit_effect.connect(world.add_child)
 	Events.orb_dropped.connect(world.add_child, CONNECT_DEFERRED)
 	Events.loot_dropped.connect(world.add_child, CONNECT_DEFERRED)
+	Events.player_hit.connect(world.add_child)
