8412ce40f2dd9b16e2258b3f7ab113d5ba2e3a1c
juliohq <JulioHenrique31501234@gmail.com>
2026-05-14 18:20:52 -0300

    Add audio bus enum

--- scenes/audio.gd
+++ scenes/audio.gd
@@ -1,9 +1,16 @@
 extends Node
 
 
+enum Bus {
+	AUDIO,
+	MUSIC,
+}
+
 @export var audio: AudioStream
 @export_range(0.0, 2.0, 0.01, "or_greater") var audio_volume := 1.0
+@export var bus := Bus.AUDIO
 
 
 func _ready() -> void:
-	AudioManager.play(audio, audio_volume, "Sounds")
+	var bus_name := "Sounds" if bus == Bus.AUDIO else "Music"
+	AudioManager.play(audio, audio_volume, bus_name)
