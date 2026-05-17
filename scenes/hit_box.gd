8e5bfeaf3db6ae60f2e8b4868e6774843660624e
juliohq <JulioHenrique31501234@gmail.com>
2026-05-17 09:03:53 -0300

    Remove freeze touch damage

--- scenes/hit_box.gd
+++ scenes/hit_box.gd
@@ -8,7 +8,7 @@ signal hit(body: Node2D)
 ## otherwise, attack will be manual.
 @export var on_enter := true
 ## How much health this hitbox will take from enemies.
-@export_range(1, 10, 1, "or_greater", "suffix:HP") var attack := 1
+@export_range(0, 10, 1, "or_greater", "suffix:HP") var attack := 1
 
 
 func _ready() -> void:
