87819469a420e4c55cb90bff0820b8c463c9316c
juliohq <JulioHenrique31501234@gmail.com>
2026-05-27 08:50:15 -0300

    Add coins cheat

--- autoloads/globals.gd
+++ autoloads/globals.gd
@@ -94,6 +94,10 @@ func _ready() -> void:
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed(&"ui_cancel"):
 		get_tree().quit()
+	elif OS.is_debug_build():
+		if event.is_action_pressed(&"cheat_coins"):
+			coins += 1000
+			Events.coins_changed.emit()
 
 
 ## Saves the game state to the given file path.
