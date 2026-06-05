f9784a5441cf20a6e60b7f2e652186b3e30bb34a
juliohq <JulioHenrique31501234@gmail.com>
2026-06-05 09:17:17 -0300

    Refactor offscreen deleter

--- scenes/offscreen_deleter.gd
+++ scenes/offscreen_deleter.gd
@@ -1,9 +1,16 @@
 extends VisibleOnScreenNotifier2D
 
 
+## The parent node to be removed.
 @export var parent: Node
 
+## The node entered the screen at least once.
 var entered := false
+
+
+func _ready() -> void:
+	screen_entered.connect(_screen_entered)
+	screen_exited.connect(_screen_exited)
 
 
 func _screen_entered() -> void:
