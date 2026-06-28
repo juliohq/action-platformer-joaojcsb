200b2308651b2ac2bcd952efb77c83bdad10981e
juliohq <JulioHenrique31501234@gmail.com>
2026-06-28 20:36:34 -0300

    Add music transition

--- autoloads/music_stream_player.gd
+++ autoloads/music_stream_player.gd
@@ -5,9 +5,13 @@ extends Node
 @export var autoplay := true
 @export_range(0, 600, 0.01, "or_greater") var play_after := 0.0
 @export var stop_on_exit := false
+@export var boss := false
 
 
 func _ready() -> void:
+	if boss:
+		Events.boss_defeated.connect(_boss_defeated)
+	
 	if autoplay:
 		if play_after > 0.0:
 			await get_tree().create_timer(play_after, false).timeout
@@ -17,9 +21,19 @@ func _ready() -> void:
 
 func _exit_tree() -> void:
 	if stop_on_exit:
-		Music.play_music(null)
+		stop()
 
 
 ## Plays the music stream associated with this player.
 func play() -> void:
 	Music.play_music(stream)
+
+
+## Stops the music stream.
+func stop() -> void:
+	Music.play_music(null)
+
+
+func _boss_defeated() -> void:
+	var tween := create_tween()
+	tween.tween_property(Music, "volume_linear", 0.0, 3.0)
