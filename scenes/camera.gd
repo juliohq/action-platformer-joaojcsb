042da6b868d7509b7a98533466283a47761cd719
juliohq <JulioHenrique31501234@gmail.com>
2026-08-11 20:43:35 -0300

    Add camera boundaries

--- scenes/camera.gd
+++ scenes/camera.gd
@@ -2,6 +2,7 @@ extends Camera2D
 
 
 @export var reset_smoothing_on_ready := true
+@export var bounds: StaticBody2D
 
 
 func _ready() -> void:
@@ -9,3 +10,7 @@ func _ready() -> void:
 		# Reset smoothing is deferred so other actions performed on the frame
 		# will take effect (e.g. change position of the player onready)
 		reset_smoothing.call_deferred()
+
+
+func _physics_process(_delta: float) -> void:
+	bounds.position = get_screen_center_position()
